207c772d5e08dd71bf1d198ea0f116fb7eef23fb
kbrookes <kelsey@thinksync.com.au>
2026-03-23 14:28:44 +1100

    v0.1.51: Fix Bricks dynamic tag rendering for term images

--- nova-core.php
+++ nova-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Nova Core
  * Description: Shared logic and components for all Nova Strategic sites.
- * Version: 0.1.50
+ * Version: 0.1.51
  * Author: Nova Strategic
  * Requires at least: 5.8
  * Requires PHP: 7.4
